aedee38982a8b036b644f13abbee4118dfe48354
sashok <sashok@book.local>
2014-07-03 18:44:58 +0300

    init table view

--- Days/ViewController.swift
+++ Days/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestSwiftCell")
+        
+        cell.text = "Баділи \(indexPath.row)"
+        cell.detailTextLabel.text = "Йо"
+        
+        return cell
+    }
 
 }
 
