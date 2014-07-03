34171abd57d23b9a5196e747c3afffd8e1089de4
sashok <sashok@book.local>
2014-07-03 17:15:24 +0300

    Initial Commit

---
+++ Days/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Days
+//
+//  Created by sashok on 03.07.14.
+//  Copyright (c) 2014 Se7enSky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
